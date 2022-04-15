d65da207be03f2a171e6fbf99cf5b5c2df926d85
KirillSychev <sychevks@yandex.ru>
2022-04-15 22:07:53 +0500

    t

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-/* jshint esversion: 6 */
-/* jshint strict: false */
-/* jshint node: true */
-/* jshint browser: true */
-/* jshint strict: global */
-'use strict';
+import './styles/index.scss';
 
 const userStack = {
     language: 'JavaScript',
     framework: 'Angular'
-};
+}
 
 const User = {
     name: 'Kirill',
     age: '21',
     ...userStack
-};
+}
+
+console.log(user);
