52ae3b38611ef8743bd11dea6a43c3d55078ab9e
KirillSychev <sychevks@yandex.ru>
2022-04-17 23:33:05 +0500

    header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+//import './styles/bootstrap/bootstrap.min.css';
+//import './styles/bootstrap/bootstrap-grid.min.css';
 import './styles/index.scss';
 
 const userStack = {
-    language: 'JavaScript',
-    framework: 'Angular'
+    language: 'JavaScript'
 }
 
 const User = {
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-let mode = 'development'
+let mode = 'development';
 if (process.env.NODE_ENV === 'production') {
     mode = 'production';
 }
